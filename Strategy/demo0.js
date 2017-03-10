7c905ef3a8b3210beba68f653f77c48ed27ae340
Sparetire <s9ar371r3@gmail.com>
2017-03-10 17:26:37 +0800

    add: Strategy

---
+++ Strategy/demo0.js
@@ -0,0 +1,73 @@
+let tween = {
+	linear(t, b, c, d) {
+		// console.log('flag');
+		return c * t / d + b;
+	},
+	easeIn(t, b, c, d) {
+		// console.log('flag');
+		return c * (t /= d) * t + b;
+	},
+	strongEaseIn(t, b, c, d) {
+		// console.log('flag');
+		return c * (t /= d) * t * t * t * t + b;
+	},
+	strongEaseOut(t, b, c, d) {
+		// console.log('flag');
+		return c * ((t = t/d - 1) * t * t * t * t + 1) + b;
+	},
+	sineaseIn(t, b, c, d) {
+		// console.log('flag');
+		return c * (t /= d) * t * t + b;
+	},
+	sineaseOut(t, b, c, d) {
+		// console.log('flag');
+		return c * ((t = t/d - 1) * t * t + 1) + b;
+	}
+};
+
+function Animate(dom) {
+	this.dom = dom;
+	this.startTime = 0;
+	this.startPos = 0;
+	this.endPos = 0;
+	this.propertyName = null;
+	this.easing = null;
+	this.duration = null;
+}
+
+Animate.prototype.start = function (propertyName, endPos, duration, easing) {
+	this.startTime = +new Date;
+	this.startPos = this.dom.getBoundingClientRect()[propertyName];
+	this.propertyName = propertyName;
+	this.endPos = endPos;
+	this.duration = duration;
+	this.easing = tween[easing];
+	let self = this;
+	let timeId = setInterval(() => {
+		if (self.step() === false) {
+			clearInterval(timeId);
+		}
+	}, 19);
+};
+
+Animate.prototype.step = function () {
+	let t = +new Date;
+	if (t >= this.startTime + this.duration) {
+		this.update(this.endPos);
+		return false;
+	}
+	let pos = this.easing(t - this.startTime, this.startPos, this.endPos - this.startPos, this.duration);
+	console.log(pos);
+	this.update(pos);
+};
+
+Animate.prototype.update = function (pos) {
+	console.log('flag');
+	this.dom.style[this.propertyName] = pos + 'px';
+};
+
+window.onload = () => {
+	let ball = document.getElementsByClassName('test')[0];
+	let animation = new Animate(ball);
+	animation.start('left', 500, 1000, 'easeIn');
+};
